481ef9446f94715b81c337ebc250d634fbde1092
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-01 01:28:37 +0200

    Db implementation

--- src/app.js
+++ src/app.js
@@ -1,74 +1,89 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const ejs = require("ejs");
-const _ = require('lodash');
-const port = 3000;
+const path = require('path')
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const _ = require('lodash')
+const mongoose = require('mongoose')
+const port = 3000
 
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 
-app.set("view engine", "ejs");
-app.set("views", viewsPath);
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(publicDirectoryPath));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(publicDirectoryPath))
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+//database coonnect
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+const postsSchema = {
+  title: String,
+  post: String
+}
+
+const Post = mongoose.model('Post', postsSchema)
+
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-app.get("/", (req, res) => {
-  res.render("home", {
-    startContent: homeStartingContent,
-    content: posts,
-  });
-});
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    content: aboutContent,
-  });
-});
+app.get('/', (req, res) => {
+  Post.find({}, (err, posts) => {
+    if(err){
+      console.log(err)
+    }
+    res.render('home', {
+      content: posts
+    })
+  })
+})
 
-app.get("/contact", (req, res) => {
-  res.render("contact", {
-    content: contactContent,
-  });
-});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    content: aboutContent
+  })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    content: contactContent
+  })
+})
 
 app.get('/compose', (req, res) => {
-
   res.render('compose')
 })
 
 app.post('/publish', (req, res) => {
-  posts.push({
+  const post = new Post( {
     title: req.body.title,
     post: req.body.post
   })
-  res.redirect('/')
+  post.save(()=>{
+    res.redirect('/')
+  })
+  
 })
 
 app.get('/posts/:post', (req, res) => {
-  posts.forEach(post => {
-    let param = _.lowerCase(req.params.post)
-    if (_.lowerCase(post.title) == param) {
-      res.render("post", {
-        title: post.title,
-        post: post.post
-      })
-    }
+  Post.findById({_id:req.params.post},(err,post)=>{
+    res.render('post', {
+      title: post.title,
+      post: post.post
+    })
   })
+
 })
 
-
-
 app.listen(port, () => {
-  console.log(`App is running on port:${port}`);
-});
+  console.log(`App is running on port:${port}`)
+})
